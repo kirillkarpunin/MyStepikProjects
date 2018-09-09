ee215f205e523529cdf63ba8fd6397e5b6bbc0a3
Kirill Karpunin <kirill.karpuni2014@yandex.ru>
2018-09-09 16:53:25 +0300

    Add solution for 1.8.2 task

---
+++ 1.8.2.py
@@ -0,0 +1,9 @@
+#https://stepik.org/lesson/2232/step/8?unit=929
+time = int(input())
+hours = int(input())
+minuts = int(input())
+all_time = time + (hours * 60) + minuts
+n_hours = all_time // 60
+n_minuts = all_time % 60 
+print(n_hours)
+print(n_minuts)
